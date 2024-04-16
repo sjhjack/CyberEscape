78411182ee5c688ffbfa091c7d72ea456241a8a1
DESKTOP-LL832F4\SSAFY <wjddn0308@naver.com>
2024-04-16 17:34:42 +0900

    Initial commit

---
+++ escape-be/src/main/java/com/cyber/escape/global/common/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.cyber.escape.global.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
