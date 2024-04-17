ff86cca4afb07c8d7075435dbf30a3e51ea612ad
김병주 <qudwn2941@naver.com>
2024-04-17 14:18:57 +0900

    Feat: install libraries

---
+++ escape-fe/ssafy-escape/src/app/game/components/Container.tsx
@@ -0,0 +1,13 @@
+"use client";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+export default Container;
