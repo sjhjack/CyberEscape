7c071da99806492277a8cb845d6dcefdb04ceee0
BaeJinHyeon <compuet33@gmail.com>
2024-04-17 15:22:51 +0900

    feat : config 설정 추가

--- escape-be/src/main/java/com/cyber/escape/EscapeApplication.java
+++ escape-be/src/main/java/com/cyber/escape/EscapeApplication.java
@@ -2,10 +2,12 @@ package com.cyber.escape;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing //리스너 넣을 때 사용
+@PropertySource("classpath:/config-dev.yml")
 public class EscapeApplication {
 
 	public static void main(String[] args) {
