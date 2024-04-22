6ebff2b0c6e501e81b5a1cf501be406a38aba950
김병주 <qudwn2941@naver.com>
2024-04-22 18:00:08 +0900

    feat: create mode select page

--- escape-fe/ssafy-escape/src/app/game/mode/page.tsx
+++ escape-fe/ssafy-escape/src/app/game/mode/page.tsx
@@ -1,9 +1,20 @@
 import Container from "../../../components/common/Container";
+import "./style.css";
 
 const GamePage = () => {
   return (
     <Container>
       <h1>모드 선택</h1>
+      <main>
+        <div>
+          <img src="/image/single.png" />
+          <p>싱글</p>
+        </div>
+        <div>
+          <img src="/image/multi.png" />
+          <p>멀티</p>
+        </div>
+      </main>
     </Container>
   );
 };
