5ccad7681d3a92fad6db2820a74d794c2f54614e
김병주 <qudwn2941@naver.com>
2024-04-23 13:03:49 +0900

    feat: 2 modes onclick event router

--- escape-fe/ssafy-escape/src/app/game/mode/page.tsx
+++ escape-fe/ssafy-escape/src/app/game/mode/page.tsx
@@ -1,18 +1,39 @@
+"use client";
+
 import Container from "../../../components/common/Container";
 import "./style.css";
-
+import { useRouter } from "next/navigation";
 const GamePage = () => {
+  const router = useRouter();
   return (
     <Container>
       <h1>모드 선택</h1>
       <main>
-        <div>
+        <div
+          className="selectmode"
+          onClick={() => {
+            router.push("/game/theme");
+          }}
+        >
           <img src="/image/single.png" />
-          <p>싱글</p>
+          <p>
+            싱글
+            <br />
+            (1인)
+          </p>
         </div>
-        <div>
+        <div
+          className="selectmode"
+          onClick={() => {
+            router.push("/game/multi");
+          }}
+        >
           <img src="/image/multi.png" />
-          <p>멀티</p>
+          <p>
+            멀티
+            <br />
+            (2인)
+          </p>
         </div>
       </main>
     </Container>
