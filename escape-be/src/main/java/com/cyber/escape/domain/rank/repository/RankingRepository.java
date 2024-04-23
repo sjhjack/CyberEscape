b65d2e657715f34e3199dd07d44908d54b69ee55
DESKTOP-LL832F4\SSAFY <wjddn0308@naver.com>
2024-04-23 12:30:55 +0900

    refactor: member를 user로 변경

--- escape-be/src/main/java/com/cyber/escape/domain/rank/repository/RankingRepository.java
+++ escape-be/src/main/java/com/cyber/escape/domain/rank/repository/RankingRepository.java
@@ -5,5 +5,5 @@ import com.cyber.escape.domain.rank.entity.Ranking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RankingRepository extends JpaRepository<Ranking, Long> {
+
 }
-//v2 ranking에 nickname 추가
