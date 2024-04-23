83f98305a915e637a8ee0d9bb003086ebf008072
DESKTOP-LL832F4\SSAFY <wjddn0308@naver.com>
2024-04-23 12:30:55 +0900

    refactor: member를 user로 변경

--- escape-be/src/main/java/com/cyber/escape/domain/member/repository/UserRepository.java
+++ escape-be/src/main/java/com/cyber/escape/domain/member/repository/UserRepository.java
@@ -5,9 +5,9 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.cyber.escape.domain.member.entity.Member;
+import com.cyber.escape.domain.member.entity.User;
 
 @Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-	Optional<Member> findMemberByUuid(String uuid);
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findUserByUuid(String uuid);
 }
