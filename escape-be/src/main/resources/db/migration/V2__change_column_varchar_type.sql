e8bd2bff9ba0c020acd9fa932b5d0dba3086af65
BaeJinHyeon <compuet33@gmail.com>
2024-04-23 14:26:29 +0900

    fix: uuid default 설정 및 column type 수정

--- escape-be/src/main/resources/db/migration/V2__change_column_varchar_type.sql
+++
@@ -1,6 +0,0 @@
-
-ALTER TABLE thema MODIFY description varchar(2500);
-ALTER TABLE quiz MODIFY content varchar(5000);
-ALTER TABLE chat_message MODIFY content varchar(512);
-
-ALTER TABLE user MODIFY point int NOT NULL DEFAULT 0;
