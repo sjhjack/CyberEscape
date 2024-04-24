f4a1a5b4447f4815e4ed85901476087492d8d119
ohiju <ohiju@naver.com>
2024-04-24 13:24:04 +0900

    feat: 퀴즈 모달 생성
    Former-commit-id: 04ce62ea10a71093330e38b719944d95c8400ff1

--- escape-fe/ssafy-escape/src/app/page.tsx
+++ escape-fe/ssafy-escape/src/app/page.tsx
@@ -8,11 +8,12 @@ import {
 } from "react";
 import { useGLTF } from "@react-three/drei";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import type { NextPage } from "next";
 import styled from "styled-components";
-import * as THREE from "three";
 import { Mesh } from "three";
 import { PointerLockControls as PointerLockControlsImpl } from "three/examples/jsm/controls/PointerLockControls.js";
+import CameraKeyControls from "@/components/ingame/CameraControl";
+import Modal from "@/components/ingame/QuizModal";
+import type { NextPage } from "next";
 
 interface KeyProps {
   onClick: () => void;
@@ -20,167 +21,6 @@ interface KeyProps {
 
 interface RoomProps {
   onLoaded: (isLoaded: boolean) => void;
-};
-
-interface ModalProps {
-  onClose: () => void;
-  onHideObject: () => void;
-};
-
-const CameraKeyControls = () => {
-  const { camera } = useThree();
-  const positionRef = useRef(new THREE.Vector3(4, 3, -2));
-  const [position, setPosition] = useState(new THREE.Vector3(4, 3, -2));
-  const [moveDirection, setMoveDirection] = useState({
-    forward: false,
-    backward: false,
-    left: false,
-    right: false,
-  });
-
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      switch (event.key.toLowerCase()) {
-        case "w":
-          setMoveDirection((prev) => ({ ...prev, forward: true }));
-          break;
-        case "s":
-          setMoveDirection((prev) => ({ ...prev, backward: true }));
-          break;
-        case "a":
-          setMoveDirection((prev) => ({ ...prev, left: true }));
-          break;
-        case "d":
-          setMoveDirection((prev) => ({ ...prev, right: true }));
-          break;
-        default:
-          break;
-      }
-    };
-
-    const handleKeyUp = (event: KeyboardEvent) => {
-      switch (event.key.toLowerCase()) {
-        case "w":
-          setMoveDirection((prev) => ({ ...prev, forward: false }));
-          break;
-        case "s":
-          setMoveDirection((prev) => ({ ...prev, backward: false }));
-          break;
-        case "a":
-          setMoveDirection((prev) => ({ ...prev, left: false }));
-          break;
-        case "d":
-          setMoveDirection((prev) => ({ ...prev, right: false }));
-          break;
-        default:
-          break;
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, []);
-
-  useEffect(() => {
-    let animationFrameId: number;
-
-    const updatePosition = () => {
-      let direction = new THREE.Vector3();
-      camera.getWorldDirection(direction);
-      direction.normalize();
-
-      let left = new THREE.Vector3();
-      left.crossVectors(new THREE.Vector3(0, 1, 0), direction).normalize();
-
-      let newPosition = position.clone();
-      positionRef.current = newPosition;
-
-      if (moveDirection.forward) {
-        newPosition.addScaledVector(direction, 0.05);
-      }
-      if (moveDirection.backward) {
-        newPosition.addScaledVector(direction, -0.05);
-      }
-      if (moveDirection.left) {
-        newPosition.addScaledVector(left, 0.05);
-      }
-      if (moveDirection.right) {
-        newPosition.addScaledVector(left, -0.05);
-      }
-
-      newPosition.y = position.y;
-
-      if (!position.equals(newPosition)) {
-        setPosition(newPosition);
-        camera.position.set(newPosition.x, newPosition.y, newPosition.z);
-      }
-
-      animationFrameId = requestAnimationFrame(updatePosition);
-    };
-
-    const isMoving = Object.values(moveDirection).some((value) => value);
-    if (isMoving) {
-      animationFrameId = requestAnimationFrame(updatePosition);
-    }
-
-    return () => {
-      if (animationFrameId) {
-        cancelAnimationFrame(animationFrameId);
-      }
-    };
-  }, [camera, position, moveDirection]);
-
-  return null;
-};
-
-const Modal = ({ onClose, onHideObject }: ModalProps) => {
-  const [inputValue, setInputValue] = useState("");
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
-  };
-
-  const handleSubmit = () => {
-    if (inputValue === "3") {
-      alert("정답입니다!");
-      onClose();
-      onHideObject();
-      document.dispatchEvent(new Event("modalClose"));
-      window.pointerLockControls?.lock();
-    } else {
-      alert("틀렸습니다.");
-      setInputValue("");
-    }
-  };
-
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top: "20%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        background: "white",
-        padding: "20px",
-        zIndex: 100,
-      }}
-    >
-      <div>문제 1(난이도5)</div>
-      <div>16+9 = 1 8+6 = 2 14+13 = 3 4+11 = ?</div>
-      <input
-        placeholder="정답을 입력해주세요."
-        value={inputValue}
-        onChange={handleInputChange}
-      />
-      <button onClick={handleSubmit}>제출</button>
-      <button onClick={onClose}>닫기</button>
-    </div>
-  );
 };
 
 function GLTFModel({ onLoaded }: RoomProps) {
@@ -294,7 +134,7 @@ const PointerLockControls = forwardRef((props, ref) => {
   return null;
 });
 
-type PointerLockControlsMethods = {
+interface PointerLockControlsMethods {
   moveToPosition: (x: number, y: number, z: number) => void;
 };
 
