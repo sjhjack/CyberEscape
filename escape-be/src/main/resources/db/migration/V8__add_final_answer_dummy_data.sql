89ef01744affdbf049ff45e4f4ba55718f570d12
BaeJinHyeon <compuet33@gmail.com>
2024-04-24 14:14:53 +0900

    refactor: 랜덤 문제 출제를 query로 수행하도록 로직 수정

---
+++ escape-be/src/main/resources/db/migration/V8__add_final_answer_dummy_data.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO final_answer(answer) VALUES("정우야 하루라도 농구를 안하면 덧나니");
+INSERT INTO final_answer(answer) VALUES("희주야 들어와");
+INSERT INTO final_answer(answer) VALUES("역시 지원이야");
+INSERT INTO final_answer(answer) VALUES("정훈아 웹소켓 어서 구현해줘");
+INSERT INTO final_answer(answer) VALUES("병주 넌 참 사람이 됐다");
