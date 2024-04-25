d6a0c89e0cf74361968b90cce6c0307216486e4a
김병주 <qudwn2941@naver.com>
2024-04-25 12:30:13 +0900

    feat: 캐러셀 컴포넌트화

--- escape-fe/ssafy-escape/src/app/(isLogIn)/game/theme/page.tsx
+++ escape-fe/ssafy-escape/src/app/(isLogIn)/game/theme/page.tsx
@@ -2,15 +2,26 @@
 import React, { useState } from "react"
 import Container from "@/components/common/Container"
 import { useRouter } from "next/navigation"
-import Carousel from "@/components/common/ThemeCarousel"
+import ThemeCarousel from "@/components/common/ThemeCarousel"
 const Page = () => {
   const router = useRouter()
   const [theme, selectTheme] = useState<number>(0)
 
   return (
-    <Container>
+    <Container
+      display="flex"
+      justifyContent="center"
+      flexDirection="column"
+      alignItems="center"
+    >
       <h1>테마 선택</h1>
-      <Carousel selectTheme={selectTheme} />
+      <ThemeCarousel
+        selectTheme={selectTheme}
+        width={500}
+        height={350}
+        navigation={true}
+        pagination={false}
+      />
       <button
         onClick={() => {
           console.log("게임시작", theme)
--- escape-fe/ssafy-escape/src/components/common/ThemeCarousel.tsx
+++ escape-fe/ssafy-escape/src/components/common/ThemeCarousel.tsx
@@ -1,63 +1,75 @@
 "use client"
 import React, { useState, useEffect } from "react"
-import { Navigation } from "swiper/modules"
+import { Navigation, Pagination } from "swiper/modules"
 import { Swiper, SwiperSlide } from "swiper/react"
-import CardComponent from "./ThemeCard"
+import ThemeCard from "./ThemeCard"
 import styled from "styled-components"
 import "swiper/css"
 import "swiper/css/navigation"
+import "swiper/css/pagination"
+
+// 어떤 테마를 선택했는지 파악하기 위해 인덱스로 정보 전달
 interface SelectThemeFunction {
   (index: number): void
 }
 
 interface CarouselProps {
   selectTheme: SelectThemeFunction
-  width?: string
-  height?: string
+  width?: number
+  height?: number
   navigation?: boolean
   pagination?: boolean
 }
-const ThemeCarousel = ({ selectTheme, width, height }: CarouselProps) => {
+const themeData: CardInfo[] = [
+  {
+    title: "공포",
+    time: "10-15분",
+    content: "무서운 거임",
+    image: "/image/horror.jpg",
+  },
+  {
+    title: "SSAFY",
+    time: "10-15분",
+    content: "탈출 마렵다",
+    image: "/image/ssafy.jpg",
+  },
+  {
+    title: "일반",
+    time: "10-15분",
+    content: "희주야 들어와",
+    image: "/image/normal.jpg",
+  },
+]
+const ThemeCarousel = ({
+  selectTheme,
+  width,
+  height,
+  navigation,
+  pagination,
+}: CarouselProps) => {
   const [currentCard, setCurrentCard] = useState<number>(0)
-  const themeData: CardInfo[] = [
-    {
-      title: "공포",
-      time: "10-15분",
-      content: "무서운 거임",
-      image: "/image/horror.jpg",
-    },
-    {
-      title: "SSAFY",
-      time: "10-15분",
-      content: "탈출 마렵다",
-      image: "/image/ssafy.jpg",
-    },
-    {
-      title: "일반",
-      time: "10-15분",
-      content: "희주야 들어와",
-      image: "/image/normal.jpg",
-    },
-  ]
+
   useEffect(() => {
     selectTheme(currentCard)
   }, [currentCard])
   return (
-    <MainContainer>
+    <MainContainer style={{ borderRadius: "20px" }}>
       <Swiper
-        modules={[Navigation]}
+        modules={[Navigation, Pagination]}
         slidesPerView={1}
         threshold={1}
-        onSlideChange={(prop) => setCurrentCard(prop.activeIndex)}
+        onSlideChange={(e) => setCurrentCard(e.activeIndex)}
         spaceBetween={100}
         centeredSlides={true}
-        navigation={true}
-        pagination={true}
+        navigation={navigation ? navigation : false}
+        pagination={
+          pagination ? (pagination === true ? { clickable: true } : {}) : false
+        }
       >
         {themeData?.map((item: CardInfo, index: number) => {
           return (
             <SwiperSlide key={index}>
-              <CardComponent themeData={item} />
+              <ThemeCard themeData={item} width={width} height={height} />
             </SwiperSlide>
           )
         })}
