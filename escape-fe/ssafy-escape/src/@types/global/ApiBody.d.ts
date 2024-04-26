727d5e512545262ac7f7fb2b5cac751433595829
류지원 <jiwoni1210@naver.com>
2024-04-26 12:54:07 +0900

    refactor: 파일구조변경 및 기타수정

---
+++ escape-fe/ssafy-escape/src/@types/global/ApiBody.d.ts
@@ -0,0 +1,5 @@
+interface NullBodyProps {
+  status: number
+  message: string
+  data: null
+}
---
+++ escape-fe/ssafy-escape/src/app/(isLogIn)/main/friends/friendsStyle.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const TitleText = styled.div`
+  font-size: 25px;
+  text-align: center;
+  position: relative;
+  font-weight: 700;
+`
+
+export const MainContainer = styled.div`
+  padding: 4vh 13vw;
+`
+
+export const ButtonBox = styled.div`
+  position: absolute;
+  top: -3px;
+  right: 0px;
+  left: 200px;
+`
