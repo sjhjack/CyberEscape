646a327c23a025f708914067bf5aef1f9eb22da5
김병주 <qudwn2941@naver.com>
2024-04-30 14:29:18 +0900

    feat: stomp.js 설치
    Former-commit-id: bcdffa70de93e6405b0c6eb523b855d350d2a5de

--- escape-fe/ssafy-escape/src/components/game/multi/waiting/Waiting.tsx
+++ escape-fe/ssafy-escape/src/components/game/multi/waiting/Waiting.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import Container from "@/components/common/Container"
 import * as S from "../../../../app/(isLogIn)/game/multi/waiting/waitingStyle"
 import ChattingBox from "@/components/game/multi/waiting/ChattingBox"
@@ -7,6 +7,17 @@ import Openvidu from "./Openvidu"
 
 const Waiting = () => {
   const [session, setSession] = useState(null)
+  const [userType, setUserType] = useState(null)
+  const [roomUuid, setRoomUuid] = useState(null)
+  const [userUuid, setUserUuid] = useState(null)
+  const [isHost, setIsHost] = useState(false)
+  const webSocket = useState(null)
+  const stompClient = useState(null)
+  const room = useState({
+    host: null,
+    guest: null,
+  })
+
   return (
     <Container display="flex" justifyContent="center" alignItems="center">
       {/* <Openvidu /> */}
@@ -35,3 +46,79 @@ const Waiting = () => {
 }
 
 export default Waiting
+
+// setup() {
+//   const userType = ref('host');
+//   const roomUuid = ref('');
+//   const userUuid = ref('');
+//   const webSocket = ref(null);
+//   const stompClient = ref(null);
+//   const room = ref({
+//     host: null,
+//     guest: null,
+//   });
+//   const messages = ref([]);
+//   const kickedMessage = ref('d');
+
+//   const isHost = computed(() => userType.value === 'host');
+
+//   const connect = () => {
+//     const socket = new WebSocket(`ws://localhost:8080/ws-stomp`);
+//     webSocket.value = socket;
+//     stompClient.value = Stomp.over(socket);
+//     stompClient.value.connect({}, onConnected, onError);
+//   };
+
+//   const onConnected = () => {
+//     stompClient.value.subscribe(`/topic/${roomUuid.value}`, onRoomInfo);
+//     stompClient.value.subscribe('/user/queue/kick', onKicked);
+//     stompClient.value.send(
+//       '/pub/room.connect',
+//       {
+//         'userUuid': userUuid.value,
+//         'roomUuid': roomUuid.value,
+//         'userType': userType.value,
+//       }
+//     );
+//   };
+
+//   const onRoomInfo = (payload) => {
+//     const roomInfo = JSON.parse(payload.body);
+//     room.value = roomInfo;
+
+//     // 수신한 메시지를 messages 배열에 추가
+//     const message = `[${new Date().toLocaleTimeString()}] ${JSON.stringify(roomInfo)}`;
+//     messages.value.push(message);
+//   };
+
+//   // 수신한 메시지를 kickedMessage에 저장
+//   const onKicked = (payload) => {
+//     kickedMessage.value = payload.body;
+//   };
+
+//   const onError = (error) => {
+//     console.error(error);
+//   };
+
+//   const kickGuest = () => {
+//     stompClient.value.send('/pub/room.kickGuest', {}, roomUuid.value);
+//   };
+
+//   const delegateHost = () => {
+//     stompClient.value.send('/pub/room.delegateHost', {}, roomUuid.value);
+//   };
+
+//   return {
+//     userType,
+//     roomUuid,
+//     userUuid,
+//     room,
+//     isHost,
+//     messages,
+//     kickedMessage,
+//     connect,
+//     kickGuest,
+//     delegateHost,
+//   };
+// },
+// };
