7ea49e98b6924b1be53445f5b2d4e543f11316fa
DESKTOP-LL832F4\SSAFY <wjddn0308@naver.com>
2024-04-30 09:57:14 +0900

    feat: 친구 등록 구현 완료

--- escape-be/src/main/java/com/cyber/escape/domain/friend/dto/FriendDto.java
+++ escape-be/src/main/java/com/cyber/escape/domain/friend/dto/FriendDto.java
@@ -1,4 +1,14 @@
 package com.cyber.escape.domain.friend.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+
 public class FriendDto {
+    @Getter
+    @Builder
+    public static class friendRequest{
+        private String fromUserUuid;
+        private String toUserUuid;
+    }
+
 }
