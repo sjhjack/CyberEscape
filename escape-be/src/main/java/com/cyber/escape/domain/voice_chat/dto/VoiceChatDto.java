a7bddc2cb992ed05b79754bf7279f2add4432290
BaeJinHyeon <compuet33@gmail.com>
2024-05-02 13:00:25 +0900

    fix: No serializer 및 no parameter 에러 수정

--- escape-be/src/main/java/com/cyber/escape/domain/voice_chat/dto/VoiceChatDto.java
+++ escape-be/src/main/java/com/cyber/escape/domain/voice_chat/dto/VoiceChatDto.java
@@ -2,20 +2,36 @@ package com.cyber.escape.domain.voice_chat.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
-@Component
+@Getter
 public class VoiceChatDto {
 
     @Builder
-    @AllArgsConstructor
+    @Getter
     public static class SessionResDto{
-        private final String sessionId;
+        private String sessionId;
+
+        public SessionResDto(){
+
+        }
+        public SessionResDto(String sessionId){
+            this.sessionId = sessionId;
+        }
     }
 
     @Builder
-    @AllArgsConstructor
+    @Getter
     public static class ConnectionResDto{
         private String voiceChatToken;
+
+        public ConnectionResDto(){
+
+        }
+
+        public ConnectionResDto(String voiceChatToken){
+            this.voiceChatToken = voiceChatToken;
+        }
     }
 }
