089a1234ce47a1d92a6f21ff50241c6372607142
ohiju <ohiju@naver.com>
2024-05-02 10:48:28 +0900

    fix: basic scene 공통컴포넌트화
    Former-commit-id: 8ff8ac01ac120b0307afc1faf723f4c6d45ea578

--- escape-fe/ssafy-escape/src/components/ingame/BasicScene.tsx
+++ escape-fe/ssafy-escape/src/components/ingame/BasicScene.tsx
@@ -2,10 +2,7 @@ import { ReactNode, useState, useEffect } from "react"
 import { Canvas } from "@react-three/fiber"
 import { PointerLockControls } from "@react-three/drei"
 import { Physics } from "@react-three/cannon"
-import Lights from "./elements/space/Lights"
-import Floor from "./elements/common/Floor"
 import Crosshair from "./Crosshair"
-import MeshObjects from "./elements/space/MeshObjects"
 
 const CustomPointerLockControls = () => {
   return <PointerLockControls />
@@ -29,12 +26,7 @@ const BasicScene = ({ children }: { children: ReactNode }) => {
   return (
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
       <Canvas shadows camera={{ fov: 50 }}>
-        <Lights />
-        <Physics gravity={[0, -9.8, 0]}>
-          {children}
-          <MeshObjects />
-          <Floor rotation={[Math.PI / -2, 0, 0]} color="white" />
-        </Physics>
+        <Physics gravity={[0, -9.8, 0]}>{children}</Physics>
         <CustomPointerLockControls />
       </Canvas>
       {isPointerLocked && <Crosshair />}
