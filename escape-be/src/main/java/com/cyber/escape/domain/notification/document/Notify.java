2755286884ee9fd814dba63daa23df13e94b86ef
BaeJinHyeon <compuet33@gmail.com>
2024-05-02 16:54:30 +0900

    feat: sse 로직 구현

---
+++ escape-be/src/main/java/com/cyber/escape/domain/notification/document/Notify.java
@@ -0,0 +1,2 @@
+package com.cyber.escape.domain.notification.document;public class Notify {
+}
---
+++ escape-be/src/main/java/com/cyber/escape/domain/notification/dto/NotifyDto.java
@@ -0,0 +1,39 @@
+package com.cyber.escape.domain.notification.dto;
+
+import java.time.LocalDateTime;
+
+import com.cyber.escape.domain.notification.document.Notify;
+import lombok.Builder;
+import lombok.Getter;
+
+// @Getter
+// @Builder
+// @AllArgsConstructor
+// @NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class NotifyDto {
+    // Todo : member의 unique 값을 뭐로 저장하냐에 따라서 id 타입 바꾸기
+    // 지금은 Long id를 참조
+
+    @Getter
+    @Builder
+    public static class Response {
+        String id;
+        String receiverUuid;
+        String content;
+        String type;
+        char isRead;
+        LocalDateTime createdAt;
+
+        public static NotifyDto.Response from(Notify notify){
+            return NotifyDto.Response.builder()
+                    .id(notify.getId().toString())
+                    .receiverUuid(notify.getReceiverUuid())
+                    .content(notify.getContent())
+                    .type(notify.getNotificationType().name())
+                    .isRead(notify.getIsRead())
+                    .createdAt(notify.getCreatedAt())
+                    .build();
+        }
+    }
+}
+
