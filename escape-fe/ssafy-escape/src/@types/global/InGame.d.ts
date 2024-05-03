896bf13cd2cebed27dd2faf06fe9fc8596606911
류지원 <jiwoni1210@naver.com>
2024-05-03 15:44:57 +0900

    feat: 공포테마 연출 추가 및 폴더 생성
    Former-commit-id: 7e00110bcd474668df764b19e16d959ff39d0917

--- escape-fe/ssafy-escape/src/@types/global/InGame.d.ts
+++ escape-fe/ssafy-escape/src/@types/global/InGame.d.ts
@@ -1,4 +1,10 @@
+// 인게임 관련 인터페이스들
+
 interface IngameMainProps {
   isGameStart: boolean
   setIsModelLoaded: (isModelLoaded: boolean) => void
+}
+
+interface RoomProps {
+  onLoaded: (isLoaded: boolean) => void
 }
