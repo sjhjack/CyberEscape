b43ee11ae4ebd2ecba8135a52f6c04f8bab2b3cb
김병주 <qudwn2941@naver.com>
2024-05-03 16:05:11 +0900

    fix: usesearchparams 에러 수정
    Former-commit-id: 1d01f5df4024f30c684688e2f4eea8e7401c7f87

--- escape-fe/ssafy-escape/src/app/(isLogIn)/game/theme/page.tsx
+++ escape-fe/ssafy-escape/src/app/(isLogIn)/game/theme/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react"
 import Theme from "@/components/game/theme/Theme"
 
 const Page = () => {
-  return <Theme />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Theme />
+    </Suspense>
+  )
 }
 
 export default Page
