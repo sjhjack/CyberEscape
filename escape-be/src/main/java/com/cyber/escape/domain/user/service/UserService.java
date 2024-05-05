6fc8bce4098a8678f99c6d4d68ad368e0b2664ed
송정훈 <sjhjack@naver.com>
2024-05-05 11:20:42 +0900

    refactor: UserService를 UserService와 AuthService로 분리

--- escape-be/src/main/java/com/cyber/escape/domain/user/service/UserService.java
+++ escape-be/src/main/java/com/cyber/escape/domain/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.cyber.escape.domain.user.service;
 import com.cyber.escape.domain.user.dto.UserDto;
 
 public interface UserService {
+	// Todo : Read랑 Modify (Create, Update, Delete)로 분리하자
 	String generateNickname();
 	UserDto.SearchNicknameResponse searchNickname(UserDto.SearchNicknameRequest dto);
 	UserDto.CheckNicknameResponse checkNickname(UserDto.CheckNicknameRequest request);
