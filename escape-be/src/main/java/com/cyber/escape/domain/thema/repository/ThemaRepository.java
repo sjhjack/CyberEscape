5f12f98619295bf99173786b4f262dbd3a882fc1
BaeJinHyeon <compuet33@gmail.com>
2024-05-06 23:47:42 +0900

    feat: 테마 정보 불러오기 API 구현

--- escape-be/src/main/java/com/cyber/escape/domain/thema/repository/ThemaRepository.java
+++ escape-be/src/main/java/com/cyber/escape/domain/thema/repository/ThemaRepository.java
@@ -2,9 +2,18 @@ package com.cyber.escape.domain.thema.repository;
 
 import com.cyber.escape.domain.thema.entity.Thema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ThemaRepository extends JpaRepository<Thema, Long> {
     Optional<Thema> findByUuid(String uuid);
+
+    @Query(value = "SELECT * FROM thema " +
+                    "WHERE category IN (1, 4, 7)", nativeQuery = true)
+    List<Thema> findStandardThema();
+
 }
