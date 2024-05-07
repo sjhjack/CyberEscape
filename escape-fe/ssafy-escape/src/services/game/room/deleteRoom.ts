25a1f32fe4378863061a2256bfd8881260436e8d
김병주 <qudwn2941@naver.com>
2024-05-07 12:25:19 +0900

    feat: game room api 주석 해제
    Former-commit-id: 64792de85bcf2adccf3ab9f13eaa6c7d24eec16f

--- escape-fe/ssafy-escape/src/services/game/room/deleteRoom.ts
+++ escape-fe/ssafy-escape/src/services/game/room/deleteRoom.ts
@@ -1,28 +1,28 @@
-// import API_PATH from "@/constants/path"
-// import api from "/api"
+import API_PATH from "@/constants/path"
+import api from "@/services/api"
 
 interface deleteRoomRequestProps {
   roomUuid: string
   userUuid: string
 }
 
-// // 방 삭제하기
-// const deleteRoom = async (
-//   data: deleteRoomRequestProps,
-// ): Promise<NullBodyProps> => {
-//   try {
-//     const response = await api.delete<NullBodyProps>(
-//       API_PATH.GAME.MULTI.ROOM.LIST,
-//       data,
-//     )
-//     if (response.status === 400) {
-//       throw new Error(`오류: ${response.data.message}`)
-//     }
-//     return response.data.data
-//   } catch (error) {
-//     console.error(error)
-//     throw error
-//   }
-// }
+// 방 삭제하기
+const deleteRoom = async (
+  data: deleteRoomRequestProps,
+): Promise<NullBodyProps> => {
+  try {
+    const response = await api.delete<NullBodyProps>(
+      API_PATH.GAME.MULTI.ROOM.LIST,
+      { data: data },
+    )
+    if (response.status === 400) {
+      throw new Error(`오류: ${response.data}`)
+    }
+    return response.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
-// export default deleteRoom
+export default deleteRoom
--- escape-fe/ssafy-escape/src/services/game/room/postAcceptance.ts
+++ escape-fe/ssafy-escape/src/services/game/room/postAcceptance.ts
@@ -1,28 +1,28 @@
 import API_PATH from "@/constants/path"
-// import api from "/api"
+import api from "@/services/api"
 
 interface PostAcceptanceRequestProps {
   roomUuid: string
   userUuid: string
 }
 
-// // 게임방 초대 응하기
-// const postAcceptance = async (
-//   data: PostAcceptanceRequestProps,
-// ): Promise<NullBodyProps> => {
-//   try {
-//     const response = await api.post<NullBodyProps>(
-//       API_PATH.GAME.MULTI.ROOM.ACCEPT,
-//       data,
-//     )
-//     if (response.status === 400) {
-//       throw new Error(`오류: ${response.data.message}`)
-//     }
-//     return response.data.data
-//   } catch (error) {
-//     console.error(error)
-//     throw error
-//   }
-// }
+// 게임방 초대 응하기
+const postAcceptance = async (
+  data: PostAcceptanceRequestProps,
+): Promise<NullBodyProps> => {
+  try {
+    const response = await api.post<NullBodyProps>(
+      API_PATH.GAME.MULTI.ROOM.ACCEPT,
+      data,
+    )
+    if (response.status === 400) {
+      throw new Error(`오류: ${response.data.message}`)
+    }
+    return response.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
-// export default postAcceptance
+export default postAcceptance
