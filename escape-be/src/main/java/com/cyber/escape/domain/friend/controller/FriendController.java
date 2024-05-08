23326525ba8211546ac8d7df943588dcb44fc224
BaeJinHyeon <compuet33@gmail.com>
2024-05-08 13:04:48 +0900

    feat: 친구 목록 페이지네이션 구현

--- escape-be/src/main/java/com/cyber/escape/domain/friend/controller/FriendController.java
+++ escape-be/src/main/java/com/cyber/escape/domain/friend/controller/FriendController.java
@@ -8,10 +8,7 @@ import com.cyber.escape.global.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -37,9 +34,9 @@ public class FriendController {
     }
 
     // 친구 목록
-    @PostMapping("/list")
-    public ApiResponse<List<FriendDto.FriendListResponse>> getMyFriendList(){
-        return new ApiResponse<>(HttpStatus.OK.value(), "내 친구 목록을 가져왔습니다.", friendService.getMyFriendList());
+    @GetMapping("/list")
+    public ApiResponse<List<FriendDto.FriendListResponse>> getMyFriendList(@RequestParam int pageNumber){
+        return new ApiResponse<>(HttpStatus.OK.value(), "내 친구 목록을 가져왔습니다.", friendService.getMyFriendList(pageNumber));
     }
 
     @PostMapping("/remove")
