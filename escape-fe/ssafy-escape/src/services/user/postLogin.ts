0c298efb101f03b1585b57e9a0919b2e7db58b38
김병주 <qudwn2941@naver.com>
2024-05-08 15:04:50 +0900

    fix: 에러 상태 코드 수정
    Former-commit-id: 1ceeacbd4f58de3694af0e3aec6972fb347c06ad

--- escape-fe/ssafy-escape/src/services/user/postLogin.ts
+++ escape-fe/ssafy-escape/src/services/user/postLogin.ts
@@ -23,17 +23,13 @@ const postLogin = async (
         headers: { Authorization: `Bearer ${accessToken}` },
       },
     )
-    // 아이디 중복시 상태코드 변경(202는 임시)
+    // 아이디 중복시 상태코드 변경
     if (response.status === 400) {
       throw new Error(`오류: ${response.data.message}`)
-    } else if (response.status === 202) {
-      throw new Error("중복된 아이디입니다.")
+    } // 존재하지 않는 사용자 or 비밀번호 틀릴 시
+    else if (response.status === 403) {
+      throw new Error("사용자를 찾을 수 없습니다.")
     }
-
-    // // 존재하지 않는 사용자로 로그인 시도 시(데이터 or 상태코드 보고 수정필요)
-    // if (!response.user) {
-    //   throw new Error("사용자를 찾을 수 없습니다.")
-    // }
 
     sessionStorage.setItem("access_token", response.data.data.accessToken)
     sessionStorage.setItem("refresh_token", response.data.data.refreshToken)
