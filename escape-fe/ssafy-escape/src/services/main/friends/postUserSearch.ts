7a2398c940ea24e3a8f138ec5e135f0cd7b35e88
BaeJinHyeon <compuet33@gmail.com>
2024-05-08 17:57:29 +0900

    feat: 친구 기능 axios 설정
    Former-commit-id: ac9bde50c7f84eca91694a626f6e75f10f141145

--- escape-fe/ssafy-escape/src/services/main/friends/postUserSearch.ts
+++ escape-fe/ssafy-escape/src/services/main/friends/postUserSearch.ts
@@ -1,41 +1,45 @@
-// import API_PATH from "@/constants/path"
-// import api from "/api"
+import API_PATH from "@/constants/path"
+import api from "@/services/api"
 
-// interface PostUserSearchBodyProps {
-//   status: number
-//   message: string
-//   data: PostUserSearchDataProps[]
-// }
+interface PostUserSearchBodyProps {
+  status: number
+  message: string
+  data: PostUserSearchDataProps[]
+}
 
-// interface PostUserSearchDataProps {
-//   nickname: string
-// }
+interface PostUserSearchDataProps {
+  nickname: string
+}
 
-// // 유저 닉네임 검색
-// const postUserSearch = async (
-//   nickname: string,
-// ): Promise<PostUserSearchDataProps> => {
-//   try {
-//     const response = await api.post<PostUserSearchBodyProps>(
-//       API_PATH.MAIN.FRIEND.SEARCH,
-//       { nickname },
-//     )
-//     if (response.status === 400) {
-//       throw new Error(`오류: ${response.data.message}`)
-//     }
-//     return response.data.data
-//   } catch (error) {
-//     console.error(error)
-//     throw error
-//   }
-// }
-
-// export default postUserSearch
-
-import dummy from "./postUserSearch.json"
-const postUserSearch = async (nickname: string) => {
-  console.log(nickname)
-  return dummy
+// 유저 닉네임 검색
+const postUserSearch = async (
+  nickname: string,
+): Promise<PostUserSearchDataProps> => {
+  const accessToken = sessionStorage.getItem("access_token");
+  try {
+    const response = await api.post<PostUserSearchBodyProps>(
+      API_PATH.MAIN.FRIEND.SEARCH,
+      { nickname },
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    )
+    if (response.status === 400) {
+      throw new Error(`오류: ${response.data.message}`)
+    }
+    return response.data.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
 export default postUserSearch
+
+// import dummy from "./postUserSearch.json"
+// const postUserSearch = async (nickname: string) => {
+//   console.log(nickname)
+//   return dummy
+// }
+
+// export default postUserSearch
