c8bdc61683a8fbc09405f19d6d049097289ff765
BaeJinHyeon <compuet33@gmail.com>
2024-05-08 17:57:29 +0900

    feat: 친구 기능 axios 설정
    Former-commit-id: c35e463dd0d319d854b0010a1a2868dcf012e018

--- escape-fe/ssafy-escape/src/services/main/friends/postFriendList.ts
+++ escape-fe/ssafy-escape/src/services/main/friends/postFriendList.ts
@@ -1,38 +1,44 @@
-// import API_PATH from "@/constants/path"
-// import api from "/api"
+import API_PATH from "@/constants/path"
+import api from "@/services/api"
 
-// interface PostFriendListBodyProps {
-//   status: number
-//   message: string
-//   data: PostFriendListDataProps[]
-// }
+interface PostFriendListBodyProps {
+  status: number
+  message: string
+  data: PostFriendListDataProps[]
+}
 
-// interface PostFriendListDataProps {
-//   friendNickname: string
-//   friendUuid: string
-// }
+interface PostFriendListDataProps {
+  friendNickname: string
+  friendUuid: string
+}
 
-// // 친구 목록 조회
-// const postFriendList = async (): Promise<PostFriendListDataProps> => {
-//   try {
-//     const response = await api.post<PostFriendListBodyProps>(
-//       API_PATH.MAIN.FRIEND.LIST,
-//     )
-//     if (response.status === 400) {
-//       throw new Error(`오류: ${response.data.message}`)
-//     }
-//     return response.data.data
-//   } catch (error) {
-//     console.error(error)
-//     throw error
-//   }
-// }
+// 친구 목록 조회
+const postFriendList = async (): Promise<PostFriendListDataProps> => {
+  
+  const accessToken = sessionStorage.getItem("access_token")
+  try {
+    const response = await api.post<PostFriendListBodyProps>(
+      API_PATH.MAIN.FRIEND.LIST,
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    )
+    if (response.status === 400) {
+      throw new Error(`오류: ${response.data.message}`)
+    }
+    
+    return response.data.data
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
 // export default postFriendList
 
-import dummy from "./postFriendList.json"
-const postFriendList = async () => {
-  return dummy
-}
+// import dummy from "./postFriendList.json"
+// const postFriendList = async () => {
+//   return dummy
+// }
 
 export default postFriendList
--- escape-fe/ssafy-escape/src/services/main/friends/postFriendRequest.ts
+++ escape-fe/ssafy-escape/src/services/main/friends/postFriendRequest.ts
@@ -1,27 +1,31 @@
-// import API_PATH from "@/constants/path"
-// import api from "/api"
+import API_PATH from "@/constants/path"
+import api from "@/services/api"
 
-// // 친구 신청
-// const postFriendRequest = async (
-//   userUuid: string,
-//   friendUuid: string,
-// ): Promise<null> => {
-//   try {
-//     const response = await api.post<NullBodyProps>(
-//       API_PATH.MAIN.FRIEND.REQUEST,
-//       {
-//         userUuid,
-//         friendUuid,
-//       },
-//     )
-//     if (response.data.status === 400) {
-//       throw new Error(`오류: ${response.data.message}`)
-//     }
-//     return null
-//   } catch (error) {
-//     console.error(error)
-//     throw error
-//   }
-// }
+// 친구 신청
+const postFriendRequest = async (
+  userUuid: string,
+  friendUuid: string,
+): Promise<null> => {
+    const accessToken = sessionStorage.getItem("access_token")
+  try {
+    const response = await api.post<NullBodyProps>(
+      API_PATH.MAIN.FRIEND.REQUEST,
+      {
+        userUuid,
+        friendUuid,
+      },
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    )
+    if (response.data.status === 400) {
+      throw new Error(`오류: ${response.data.message}`)
+    }
+    return null
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
 
-// export default postFriendRequest
+export default postFriendRequest
