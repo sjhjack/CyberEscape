8eb02746a9d00ca858dde8f52910182e23c9e712
김병주 <qudwn2941@naver.com>
2024-05-08 15:04:50 +0900

    fix: 에러 상태 코드 수정
    Former-commit-id: c0c78129db849feeee9c8f8e92d601b07119664c

--- escape-fe/ssafy-escape/src/services/user/postSignUp.ts
+++ escape-fe/ssafy-escape/src/services/user/postSignUp.ts
@@ -11,8 +11,8 @@ const postSignUp = async (loginId: string, password: string): Promise<null> => {
     if (response.data.status === 404) {
       throw new Error(`오류: ${response.data.message}`)
     }
-    // 아이디 중복시 상태코드 변경(403은 임시)
-    else if (response.data.status === 403) {
+    // 아이디 중복시 상태코드 변경(500은 임시)
+    else if (response.data.status === 500) {
       throw new Error("중복된 아이디입니다.")
     }
     return null
