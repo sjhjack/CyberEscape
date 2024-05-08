81302f22e069160e0a28d251df3e26b1df66f659
김병주 <qudwn2941@naver.com>
2024-05-08 16:05:02 +0900

    feat: 닉네임 api 연결
    Former-commit-id: 1995f351216c726823b6b3302ef700cfdfc6e849

--- escape-fe/ssafy-escape/src/services/main/nickname/postAutoCreateNickname.ts
+++ escape-fe/ssafy-escape/src/services/main/nickname/postAutoCreateNickname.ts
@@ -1,39 +1,39 @@
-// import API_PATH from "@/constants/path"
-// import api from "/api"
+import API_PATH from "@/constants/path"
+import api from "@/services/api"
 
-// interface postAutoCreateNicknameBodyProps {
-//   status: number
-//   message: string
-//   data: postAutoCreateNicknameDataProps
-// }
-
-// interface postAutoCreateNicknameDataProps {
-//   words: string[]
-//   seed: string
-// }
-
-// // 닉네임 자동 생성
-// const postAutoCreateNickname =
-//   async (): Promise<postAutoCreateNicknameDataProps> => {
-//     try {
-//       const response = await api.post<postAutoCreateNicknameBodyProps>(
-//         API_PATH.MAIN.NICKNAME.AUTO_CREATE,
-//       )
-//       if (response.status === 400) {
-//         throw new Error(`오류: ${response.data.message}`)
-//       }
-//       return response.data.data[0]
-//     } catch (error) {
-//       console.error(error)
-//       throw error
-//     }
-//   }
-
-// export default postAutoCreateNickname
-
-import dummy from "./postAutoCreateNickname.json"
-const postAutoCreateNickname = async () => {
-  return dummy
+interface postAutoCreateNicknameBodyProps {
+  status: number
+  message: string
+  data: postAutoCreateNicknameDataProps
 }
 
+interface postAutoCreateNicknameDataProps {
+  words: string[]
+  seed: string
+}
+
+// 닉네임 자동 생성
+const postAutoCreateNickname =
+  async (): Promise<postAutoCreateNicknameDataProps> => {
+    try {
+      const response = await api.post<postAutoCreateNicknameBodyProps>(
+        API_PATH.MAIN.NICKNAME.AUTO_CREATE,
+      )
+      if (response.status === 400) {
+        throw new Error(`오류: ${response.data.message}`)
+      }
+      return response.data.data
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+  }
+
 export default postAutoCreateNickname
+
+// import dummy from "./postAutoCreateNickname.json"
+// const postAutoCreateNickname = async () => {
+//   return dummy
+// }
+
+// export default postAutoCreateNickname
