007613013cffe43e6a98bc0bc0605ec65914b1d6
김병주 <qudwn2941@naver.com>
2024-05-11 18:41:21 +0900

    feat: 방 입장 시 host, guest 연결 완료
    Former-commit-id: 97816a6b4f59f024d0643c60f353f09dfeaabe9e

--- escape-fe/ssafy-escape/src/components/game/multi/waiting/Waiting.tsx
+++ escape-fe/ssafy-escape/src/components/game/multi/waiting/Waiting.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState, useEffect, useRef } from "react"
 import { useRouter, usePathname } from "next/navigation"
-import { Stomp, Client } from "@stomp/stompjs"
+import * as StompJs from "@stomp/stompjs"
 import SockJS from "sockjs-client"
 import useOpenViduSession from "@/hooks/OpenviduSession"
 import Container from "@/components/common/Container"
@@ -11,7 +11,6 @@ import InviteModal from "@/components/game/multi/waiting/InviteModal"
 import Button from "@/components/common/Button"
 import useIngameThemeStore from "@/stores/IngameTheme"
 import useUserStore from "@/stores/UserStore"
-import useStompClient from "@/hooks/StompClient"
 import { CircularProgress } from "@mui/material"
 interface ChatType {
   userName: string
@@ -19,8 +18,11 @@ interface ChatType {
 }
 
 const Waiting = () => {
+  const router = useRouter()
   const pathname: string = usePathname()
   const roomUuid: string = pathname.substring(20)
+  const { accessToken, userUuid, isHost } = useUserStore()
+  const { selectedTheme } = useIngameThemeStore()
   const [chatting, setChattting] = useState<Array<ChatType>>([])
   // const { session } = useOpenViduSession(roomUuid, setChattting)
   const session = ""
@@ -28,34 +30,27 @@ const Waiting = () => {
   const handleModalClose = (): void => {
     setShowModal(false)
   }
-  const { selectedTheme } = useIngameThemeStore()
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   const baseUrl = process.env.NEXT_PUBLIC_URL
-  const { nickname, profileUrl, accessToken, userUuid } = useUserStore()
-  const client = useRef<Client | null>(null) // Ref for storing the client object
+  const client = useRef<StompJs.Client | null>(null) // Ref for storing the client object
   const [roomData, setRoomData] = useState<PubResponseData | null>(null)
-  // 콜백 함수 정의: 구독한 메시지의 데이터를 상태로 업데이트
-  const handleRoomData = (data: PubResponseData) => {
-    setRoomData(data)
-    console.log("Room data received:", data)
-  }
+
   const connectHeaders = {
     Authorization: accessToken || "",
   }
   const onConnected = () => {
     client.current?.subscribe(`/topic/${roomUuid}`, (payload) => {
-      console.log("뭐가 넘어오니", payload)
       const roomInfo = JSON.parse(payload.body)
       console.log("구독 정보", roomInfo)
       setRoomData(roomInfo)
     })
     client.current?.publish({
       destination: `/pub/room/connect`,
-      body: JSON.stringify({
-        userUuid: userUuid,
+      headers: {
+        userUuid: userUuid || "",
         roomUuid: roomUuid,
-        userType: "host",
-      }),
+        userType: isHost ? "host" : "guest",
+      },
     })
     client.current?.publish({
       destination: `/pub/room/kickGuest`,
@@ -70,30 +65,49 @@ const Waiting = () => {
       }),
     })
   }
-  useEffect(() => {
-    const newClient = useStompClient(`${baseUrl}/ws-stomp`)
-    client.current = newClient
+  //function2
+  const connect = () => {
+    client.current = new StompJs.Client({
+      webSocketFactory: () => new SockJS(`${baseUrl}/ws-stomp`),
+      connectHeaders,
+      debug: function (str) {
+        console.log(str)
+      },
+      // reconnectDelay: 5000,
+      // heartbeatIncoming: 4000,
+      // heartbeatOutgoing: 4000,
+      onConnect: () => {
+        onConnected()
+      },
+      onStompError: (frame) => {
+        console.log(`Broker reported error: ${frame.headers.message}`)
+        console.log(`Additional details: ${frame.body}`)
+      },
+    })
     client.current.activate()
-    setTimeout(() => {
-      onConnected()
-    }, 3000)
-    return () => {
-      client.current?.deactivate()
-    }
+  }
+  useEffect(() => {
+    connect()
   }, [])
 
-  // if (isLoading) {
-  //   return (
-  //     <Container
-  //       display="flex"
-  //       justifyContent="center"
-  //       alignItems="center"
-  //       backgroundColor="none"
-  //     >
-  //       <CircularProgress />
-  //     </Container>
-  //   )
-  // }
+  useEffect(() => {
+    if (roomData !== null) {
+      setIsLoading(false)
+    }
+    console.log("방 정보", roomData)
+  }, [roomData])
+  if (isLoading) {
+    return (
+      <Container
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        backgroundColor="none"
+      >
+        <CircularProgress />
+      </Container>
+    )
+  }
 
   return (
     <Container
@@ -106,13 +120,13 @@ const Waiting = () => {
       <S.UserBox style={{ marginRight: "20px" }}>
         <S.CharacterBox>
           <S.ProfileImage
-            src={profileUrl ? profileUrl : ""}
+            src={roomData?.host.profileUrl}
             alt=""
             width={100}
             height={100}
           />
         </S.CharacterBox>
-        <S.Nickname>{nickname}</S.Nickname>
+        <S.Nickname>{roomData?.host.nickname}</S.Nickname>
       </S.UserBox>
       <S.MainBox>
         <S.MainContentBox>
@@ -128,19 +142,28 @@ const Waiting = () => {
       </S.MainBox>
       <S.UserBox style={{ marginLeft: "20px" }}>
         <S.CharacterBox>
-          <S.CharacterBoxContent>
-            <Button
-              text="초대하기"
-              theme="success"
-              width="100px"
-              height="40px"
-              onClick={() => {
-                setShowModal(true)
-              }}
+          {roomData?.guest ? (
+            <S.ProfileImage
+              src={roomData?.guest.profileUrl}
+              alt=""
+              width={100}
+              height={100}
             />
-          </S.CharacterBoxContent>
+          ) : (
+            <S.CharacterBoxContent>
+              <Button
+                text="초대하기"
+                theme="success"
+                width="100px"
+                height="40px"
+                onClick={() => {
+                  setShowModal(true)
+                }}
+              />
+            </S.CharacterBoxContent>
+          )}
         </S.CharacterBox>
-        <S.Nickname></S.Nickname>
+        <S.Nickname>{roomData?.guest?.nickname}</S.Nickname>
       </S.UserBox>
     </Container>
   )
