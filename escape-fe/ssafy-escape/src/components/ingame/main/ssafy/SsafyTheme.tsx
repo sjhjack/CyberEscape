cd18bf7b0aa8c2af18920ae06a1cd58e00f4c800
김병주 <qudwn2941@naver.com>
2024-05-13 00:41:50 +0900

    feat: 싸피맵 배경음악 추가
    Former-commit-id: b2b711701f0d239b46c5305d7982a58f23eae9a6

--- escape-fe/ssafy-escape/src/components/ingame/main/ssafy/SsafyTheme.tsx
+++ escape-fe/ssafy-escape/src/components/ingame/main/ssafy/SsafyTheme.tsx
@@ -6,23 +6,36 @@ import BasicScene from "@/components/ingame/BasicScene"
 import Player from "@/components/ingame/elements/common/Player"
 import Floor from "@/components/ingame/elements/common/Floor"
 import SsafyClassRoom from "../../elements/ssafy/SsafyClassRoom"
+import CountdownTimer, { CountdownTimerHandle } from "../../CountdownTimer"
+import Start from "../../elements/ssafy/Start"
+import Subtitle from "../../elements/common/Subtitle"
 // const startPosition = { x: 8, y: 8, z: 1 }
 // const startTargetPosition = { x: 4, y: 3, z: -2 }
 // const lookAt = { x: -4, y: 2, z: 2 }
-
 const SsafyTheme = ({ isGameStart, setIsModelLoaded }: IngameMainProps) => {
+  const [subtitle, setSubtitle] = useState<string>("")
+  const timerRef = useRef<CountdownTimerHandle | null>(null)
   const [interactNum, setInteractNum] = useState<number>(1)
   return (
-    <BasicScene interactNum={interactNum}>
-      <Lights />
-      <Player position={[-3, 3, 10]} speed={20} args={[0, 0, 0]} />
-      <Floor
-        rotation={[Math.PI / -2, 0, 0]}
-        color="white"
-        position={[0, -0.5, 0]}
-      />
-      <SsafyClassRoom onLoaded={setIsModelLoaded} />
-    </BasicScene>
+    <>
+      {isGameStart ? (
+        <>
+          <CountdownTimer ref={timerRef} />
+          <Start setSubtitle={setSubtitle} />
+        </>
+      ) : null}
+      <Subtitle text={subtitle} />
+      <BasicScene interactNum={interactNum}>
+        <Lights />
+        <Player position={[-5, 7, 12]} speed={20} args={[10, 0, 0]} />
+        <Floor
+          rotation={[Math.PI / -2, 0, 0]}
+          color="white"
+          position={[0, -0.5, 0]}
+        />
+        <SsafyClassRoom onLoaded={setIsModelLoaded} />
+      </BasicScene>
+    </>
   )
 }
 
