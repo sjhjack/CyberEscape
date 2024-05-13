72e13c76199fd11e55da06b552aa729a9efcd6ed
JinHyeonBae <compuet33@naver.com>
2024-05-14 07:52:31 +0900

    feat: sse 라이브러리 추가
    Former-commit-id: ffc6bbf31a3676901de994a60020955c144420e2

--- escape-fe/ssafy-escape/src/components/EventProvider.tsx
+++ escape-fe/ssafy-escape/src/components/EventProvider.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import React from 'react';
-
+import { EventSourcePolyfill, NativeEventSource } from 'event-source-polyfill'
 interface EventProviderProps {
     children: React.ReactNode;
 }
 
 const EventProvider = ({ children }: EventProviderProps) => {
     const [messages, setMessages] = React.useState<string[]>([]);
-
+    
+    const EventSource = EventSourcePolyfill || NativeEventSource
+    const accessToken = sessionStorage.getItem("access_token");
+    
+    const eventSource = new EventSource('http://localhost:8080/notify/subscribe',{
+            headers: {
+            Authorization: `Bearer ${accessToken}`,
+            Connection: 'keep-alive',
+            },
+        }
+    );
     React.useEffect(() => {
-        const eventSource = new EventSource('http://localhost:8080/subscribe');
+        
         console.log("EVENT !!!!!");
         eventSource.onmessage = function(event) {
             console.log('New event from server:', event.data);
