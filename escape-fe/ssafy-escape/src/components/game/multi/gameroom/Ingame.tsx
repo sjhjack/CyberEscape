da366d241cd09562c44599f724f46b9671adfc3c
김병주 <qudwn2941@naver.com>
2024-05-15 21:47:14 +0900

    fix: 불필요한 파일 삭제
    Former-commit-id: f3e64cb89bf6827fc72cb00b9781c202cbdee81c

--- escape-fe/ssafy-escape/src/components/game/multi/gameroom/Ingame.tsx
+++ escape-fe/ssafy-escape/src/components/game/multi/gameroom/Ingame.tsx
@@ -13,7 +13,6 @@ import SsafyTheme from "@/components/ingame/main/ssafy/SsafyTheme"
 import useIngameThemeStore from "@/stores/IngameTheme"
 import StartScene from "@/components/ingame/StartScene"
 import HorrorTheme2 from "@/components/ingame/main/horror2/HorrorTheme2"
-import IngameStore from "@/stores/IngameStore"
 
 const Page = () => {
   const [isModelLoaded, setIsModelLoaded] = useState(false)
