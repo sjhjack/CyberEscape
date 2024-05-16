214aee9dd30bb5ff42a05778d549bb92ce893fd6
김병주 <qudwn2941@naver.com>
2024-05-16 10:25:21 +0900

    feat: 싸피테마 2(교육프로) 오브젝트 배치
    Former-commit-id: fefa3c68b7f3db3b480416d9c93d6240c7a7e1ae

--- escape-fe/ssafy-escape/src/components/ingame/main/ssafy2/SsafyTheme2.tsx
+++ escape-fe/ssafy-escape/src/components/ingame/main/ssafy2/SsafyTheme2.tsx
@@ -9,6 +9,11 @@ import SsafyOffice from "../../elements/ssafy2/SsafyOffice"
 import CountdownTimer, { CountdownTimerHandle } from "../../CountdownTimer"
 import Start from "../../elements/ssafy2/Start"
 import Subtitle from "../../elements/common/Subtitle"
+import MeshObjects from "../../elements/ssafy2/MeshObjects"
+import Plant from "../../elements/ssafy2/Plant"
+import Diary from "../../elements/ssafy2/Diary"
+import Tumbler from "../../elements/ssafy2/Tumbler"
+import useIngameSolvedStore from "@/stores/IngameQuizStore"
 // const startPosition = { x: 8, y: 8, z: 1 }
 // const startTargetPosition = { x: 4, y: 3, z: -2 }
 // const lookAt = { x: -4, y: 2, z: 2 }
@@ -16,17 +21,49 @@ const SsafyTheme2 = ({ isGameStart, setIsModelLoaded }: IngameMainProps) => {
   const [subtitle, setSubtitle] = useState<string>("")
   const timerRef = useRef<CountdownTimerHandle | null>(null)
   const [interactNum, setInteractNum] = useState<number>(1)
+  const [penalty, setPenalty] = useState<number>(0)
+  const [isGameFinished, setIsGameFinished] = useState<boolean>(false)
+  const [isTimeOut, setIsTimeOut] = useState<boolean>(false)
+  const [result, setResult] = useState<string>("")
+  const [showFirstProblem, setShowFirstProblem] = useState<boolean>(false)
+  const { solved } = useIngameSolvedStore()
+  const handleTimeOut = () => {
+    setIsTimeOut(true)
+    setResult("Timeout")
+    setIsGameFinished(true)
+  }
+  const handleFirstProblem = () => {
+    if (solved === 0) {
+      setShowFirstProblem(!showFirstProblem)
+    }
+  }
   return (
-    <BasicScene interactNum={interactNum} onAir={true}>
-      <Lights />
-      <Player position={[-3, 3, 10]} speed={20} args={[0, 0, 0]} />
-      <Floor
-        rotation={[Math.PI / -2, 0, 0]}
-        color="white"
-        position={[0, -0.5, 0]}
-      />
-      <SsafyOffice onLoaded={setIsModelLoaded} />
-    </BasicScene>
+    <>
+      {isGameStart ? (
+        <>
+          {!isGameFinished && (
+            <CountdownTimer ref={timerRef} onTimeOut={handleTimeOut} />
+          )}
+          <Start setSubtitle={setSubtitle} />
+        </>
+      ) : null}
+      <BasicScene interactNum={interactNum} onAir={true}>
+        {/* <MeshObjects /> */}
+        <Lights />
+        <Diary
+          onClick={handleFirstProblem}
+          solved={solved}
+          setInteractNum={setInteractNum}
+        />
+        <Player position={[0, 11, 0]} speed={40} args={[0, 0, 0]} />
+        <Floor
+          rotation={[Math.PI / -2, 0, 0]}
+          color="white"
+          position={[0, -0.5, 0]}
+        />
+        <SsafyOffice onLoaded={setIsModelLoaded} />
+      </BasicScene>
+    </>
   )
 }
 
