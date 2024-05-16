3a0693ac45373ef56e8c18adb77e7a6a248cbd88
오희주 <ohiju@naver.com>
2024-05-16 10:20:14 +0900

    Front/feature/우주 테마
    Former-commit-id: 1b649943bad2bde672d62afc693d10b264761ed0

--- escape-fe/ssafy-escape/src/components/ingame/BasicScene.tsx
+++ escape-fe/ssafy-escape/src/components/ingame/BasicScene.tsx
@@ -1,9 +1,10 @@
-import { ReactNode, useState, useEffect } from "react"
+import { ReactNode, useState, useEffect, useRef } from "react"
 import { Canvas } from "@react-three/fiber"
 import { PointerLockControls } from "@react-three/drei"
 import { Physics } from "@react-three/cannon"
 import PlayMusic from "./PlayMusic"
 import Crosshair from "./Crosshair"
+import styled from "styled-components"
 
 interface BasicSceneProps {
   onAir: boolean
@@ -11,20 +12,26 @@ interface BasicSceneProps {
   children: ReactNode
 }
 
-const CustomPointerLockControls = () => {
-  return (
-    <>
-      <PointerLockControls pointerSpeed={0.4} />
-    </>
-  )
-}
+const FullScreenOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  cursor: cursor;
+`
 
 const BasicScene = ({ onAir, interactNum, children }: BasicSceneProps) => {
   const [isPointerLocked, setIsPointerLocked] = useState(false)
 
+  const controlsRef = useRef<any>(null)
+
   useEffect(() => {
     const handlePointerLockChange = () => {
-      setIsPointerLocked(document.pointerLockElement !== null)
+      setIsPointerLocked(
+        document.pointerLockElement === controlsRef.current.domElement,
+      )
     }
 
     document.addEventListener("pointerlockchange", handlePointerLockChange)
@@ -34,13 +41,19 @@ const BasicScene = ({ onAir, interactNum, children }: BasicSceneProps) => {
     }
   }, [])
 
+  const handlePointerLock = () => {
+    const element = controlsRef.current.domElement
+    element.requestPointerLock()
+  }
+
   return (
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
       {/* <PlayMusic /> */}
       <Canvas shadows camera={{ fov: 50 }}>
         <Physics gravity={[0, -9.8, 0]}>{children}</Physics>
-        <CustomPointerLockControls />
+        <PointerLockControls ref={controlsRef} />
       </Canvas>
+      <FullScreenOverlay onClick={handlePointerLock}></FullScreenOverlay>
       {isPointerLocked && <Crosshair interactNum={interactNum} />}
     </div>
   )
