6558d5f3fd8d4ce7955be122c4dcc1dffd0cb2d6
김병주 <qudwn2941@naver.com>
2024-05-16 11:53:50 +0900

    fix: refresh 토큰 로직 수정
    Former-commit-id: 359b18c26d4656b56ec62b454107db2b145ef0a4

--- escape-fe/ssafy-escape/src/services/api.ts
+++ escape-fe/ssafy-escape/src/services/api.ts
@@ -1,6 +1,10 @@
 "use client"
-import axios from "axios"
+import axios, { AxiosResponse, InternalAxiosRequestConfig } from "axios"
 import useUserStore from "@/stores/UserStore"
+// AxiosRequestConfig 타입 확장
+interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
+  _retry?: boolean
+}
 const baseURL = process.env.NEXT_PUBLIC_URL
 const api = axios.create({
   baseURL: baseURL,
@@ -19,39 +23,43 @@ api.interceptors.request.use(
   (error) => Promise.reject(error),
 )
 api.interceptors.response.use(
-  (response) => response, // 성공 응답은 그대로 반환
-  async (error) => {
-    // 에러 응답이 401 (Unauthorized)인 경우에만 재시도 로직 수행
-    const originalRequest = error.config
+  async (response: AxiosResponse) => {
+    // 응답의 status가 6001인 경우 리프레시 토큰 요청을 수행
+    if (response.data.status === 6001) {
+      const originalRequest = response.config as CustomAxiosRequestConfig
+      if (!originalRequest._retry) {
+        originalRequest._retry = true // 무한 루프 방지
 
-    // 401 오류이고, retry를 위한 플래그가 없는 경우에만 재시도
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true // 무한 루프 방지
+        // 리프레시 토큰을 가져오기 위한 API 요청
+        const refreshToken = sessionStorage.getItem("refresh_token")
+        try {
+          const refreshResponse = await axios.post(
+            `${api.defaults.baseURL}/auth/refresh`,
+            {
+              refresh_token: refreshToken,
+            },
+          )
 
-      // 리프레시 토큰을 가져오기 위한 API 요청
-      const refreshToken = sessionStorage.getItem("refresh_token")
-      try {
-        const response = await axios.post(`${baseURL}/auth/refresh`, {
-          refresh_token: refreshToken,
-        })
+          // 새 액세스 토큰을 세션 스토리지에 저장
+          const newAccessToken = refreshResponse.data.access_token
+          sessionStorage.setItem("access_token", newAccessToken)
 
-        // 새 액세스 토큰을 세션 스토리지에 저장
-        const newAccessToken = response.data.access_token
-        sessionStorage.setItem("access_token", newAccessToken)
+          // 새 토큰을 기존 요청 헤더에 설정
+          originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`
 
-        // 새 토큰을 기존 요청 헤더에 설정
-        originalRequest.headers["access_token"] = newAccessToken
-
-        // 실패한 요청을 새로운 액세스 토큰으로 재시도
-        return api(originalRequest)
-      } catch (refreshError) {
-        console.error("Refresh token request failed", refreshError)
-        // 리프레시 토큰이 만료된 경우, 사용자에게 로그인 페이지로 이동 요청 또는 로그아웃 처리
-        // window.location.href = "/login"; // 필요에 따라 라우팅 처리
+          // 실패한 요청을 새로운 액세스 토큰으로 재시도
+          return api(originalRequest)
+        } catch (refreshError) {
+          console.error("Refresh token request failed", refreshError)
+          // 리프레시 토큰이 만료된 경우, 사용자에게 로그인 페이지로 이동 요청 또는 로그아웃 처리
+          // window.location.href = "/login"; // 필요에 따라 라우팅 처리
+        }
       }
     }
-    // 다른 종류의 에러에 대해서는 기본적으로 에러를 반환
-    return Promise.reject(error)
+    return response // 성공 응답은 그대로 반환
+  },
+  (error) => {
+    return Promise.reject(error) // 다른 종류의 에러에 대해서는 기본적으로 에러를 반환
   },
 )
 
